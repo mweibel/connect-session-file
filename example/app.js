1d886a0e5c579db1a9589ff496c2dab1b748c785
jKey Lu <jkeylu@gmail.com>
2013-06-22 09:34:16 +0800

    update example

--- example/app.js
+++ example/app.js
@@ -1,32 +1,35 @@
 #!/usr/bin/env node
 
-var connect = require('connect'),
-    FileSessionStore = require('../lib/connect-session-file');
+var connect = require('connect')
+  , FileStore = require('../lib/connect-file-store')(connect);
 
 connect(
-      connect.cookieParser()
-    , connect.session({ 
-          secret:'session file'
-        , store: new FileSessionStore({path:'.', printDebug:true, useAsync:true})
+  connect.cookieParser(),
+  connect.session({
+    secret:'session file',
+    store: new FileStore({
+      path:'.',
+      printDebug:true,
+      useAsync:true,
+      reapInterval: 5000,
+      maxAge: 10000
     })
-    , connect.favicon()
-    , function(req,res,next) {
-        var sess = req.session;
-        console.log('+ begin ' + req.url );
-        if (sess.views) {
-            sess.views++;
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<p>views: ' + sess.views + '</p>');
-            res.write('<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
-            res.write('<img src="blank.png"/>');
-            res.write('<img src="blank2.png"/>');
-            res.write('<img src="blank3.png"/>');
-            res.write('<img src="blank4.png"/>');
-            res.write('<img src="blank5.png"/>');
-            res.end();
-        } else {
-            sess.views = 1;
-            res.end('welcome to the file session demo. refresh!');
-        }
+  }),
+  connect.favicon(),
+  function (req, res, next) {
+    var sess = req.session;
+    console.log('+ begin ' + req.url );
+
+    if (sess.views) {
+      sess.views++;
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<p>views: ' + sess.views + '</p>');
+      res.write('<p>session cookie expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
+      res.end();
+    } else {
+      sess.views = 1;
+      res.end('welcome to the file session demo. refresh!');
     }
+  }
 ).listen(8080);
+
